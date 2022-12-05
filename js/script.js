262febceed81c998b8fd737eef1fc8d922ad448f
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-05 16:38:01 +0100

    added promt

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@
 // 4 Inserisco nell'HTML il prezzo calcolato con eventuali sconti
 
 
+// Prendo l'elemento dal DOM
+const targetElement = document.getElementById('ticket');
+// console.log(targetElement);
+
+// Chiedere i chilometri
+const userName = prompt('Quanti chilometri vuoi fare?');
+// console.log(userName);
+
+// Chiedere l'età
+const userSurName = prompt('Quanti anni hai?');
+// console.log(userSurName);
+

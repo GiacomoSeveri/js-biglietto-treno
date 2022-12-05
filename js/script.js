22c191016efb012911718416a7a6baa25526bdaf
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-05 16:35:43 +0100

    steps

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// console.log('js ok')
+
+// 1 Prendo l'elemento dal DOM
+// 2 Chiedo il numero di chilometri all'utente
+// 3 Chiedo l'età all'utente
+// 4 Inserisco nell'HTML il prezzo calcolato con eventuali sconti
+
+

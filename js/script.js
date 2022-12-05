b5e43b895e89d47025b54cc86a64d1a0516e44d8
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-05 16:55:35 +0100

    if non responsive

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 // console.log('js ok')
 
 // 1 Prendo l'elemento dal DOM
-// 2 Chiedo il numero di chilometri all'utente
-// 3 Chiedo l'età all'utente
-// 4 Inserisco nell'HTML il prezzo calcolato con eventuali sconti
+// 2 Creo la costante del prezzo
+// 3 Chiedo il numero di chilometri all'utente
+// 4 Chiedo l'età all'utente
+// 5 Calcolo il prezzo
+// 6 Inserisco nell'HTML il prezzo calcolato con eventuali sconti
 
 
 // Prendo l'elemento dal DOM
 const targetElement = document.getElementById('ticket');
 // console.log(targetElement);
 
+// Creo la costante del prezzo
+const price = 0.21;
+
 // Chiedere i chilometri
-const userName = prompt('Quanti chilometri vuoi fare?');
-// console.log(userName);
+const kilometres = prompt('Quanti chilometri vuoi fare?' , '50');
+// console.log(kilometres);
 
 // Chiedere l'età
-const userSurName = prompt('Quanti anni hai?');
-// console.log(userSurName);
+const age = prompt('Quanti anni hai?' , '18');
+// console.log(age);
 
+// 5 Calcolo il prezzo
+const totalPrice = price * kilometres;
+
+if (age < 18) {
+    totalPrice * 0.20;
+} 
+else if (age > 65) {
+    totalPrice * 0.40;
+}
+else {
+    totalPrice;
+}
+
+// Inseriamo il isultato nell'HTML
+targetElement.innerText = `Hai deciso di fare ${kilometres} KM, Hai ${age} anni: Il tuo biglietto costa ${totalPrice}€.`;
